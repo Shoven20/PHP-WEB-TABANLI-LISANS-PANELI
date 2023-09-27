c144e4bf965938b44989c2e84cec24371fcfdab8
SHOVEN <88746889+Shoven20@users.noreply.github.com>
2023-09-27 23:28:14 +0300

    Add files via upload

---
+++ C++/LoginSystem/Auth.h
@@ -0,0 +1,114 @@
+#include <windows.h>
+#include <iostream>
+#include <sstream>
+#include <WinInet.h>
+#pragma comment(lib, "wininet.lib")
+#include "xor.h"
+
+std::string roatatealphabet(const std::string& input) {
+    std::string output = input;
+    for (char& c : output)
+        if (std::isalpha(c))
+            if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+                char base = (std::isupper(c)) ? 'A' : 'a';
+                c = ((c - base + 13) % 26) + base;
+            }
+    return output;
+}
+const std::string base64_chars = _xor_("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/").c_str();
+bool is_base64(unsigned char c) {
+    return (isalnum(c) || (c == '+') || (c == '/'));
+}
+std::string base64_decode(const std::string& encoded) {
+    size_t in_len = encoded.size();
+    size_t i = 0;
+    size_t j = 0;
+    int in_ = 0;
+    unsigned char char_array_4[4], char_array_3[3];
+    std::string decoded;
+    while (in_len-- && (encoded[in_] != '=') && is_base64(encoded[in_])) {
+        char_array_4[i++] = encoded[in_]; in_++;
+        if (i == 4) {
+            for (i = 0; i < 4; i++) char_array_4[i] = base64_chars.find(char_array_4[i]);
+            char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+            char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+            char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+            for (i = 0; i < 3; i++) decoded += char_array_3[i];
+            i = 0;
+        }
+    }
+    if (i > 0) {
+        for (j = i; j < 4; j++) char_array_4[j] = 0;
+        for (j = 0; j < 4; j++) char_array_4[j] = base64_chars.find(char_array_4[j]);
+        char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+        char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+        for (j = 0; j < i - 1; j++)decoded += char_array_3[j];
+    }
+
+    return decoded;
+}
+std::string base64_encode(const std::string& input) {
+    std::string encoded;
+    size_t i = 0;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
+    for (const auto& c : input) {
+        char_array_3[i++] = c;
+        if (i == 3) {
+            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+            char_array_4[3] = char_array_3[2] & 0x3f;
+            for (size_t j = 0; j < 4; j++) encoded += base64_chars[char_array_4[j]];
+            i = 0;
+        }
+    }
+    if (i > 0) {
+        for (size_t j = i; j < 3; j++) char_array_3[j] = '\0';
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+        for (size_t j = 0; j < i + 1; j++) encoded += base64_chars[char_array_4[j]];
+        while (i++ < 3) encoded += '=';
+    }
+    return encoded;
+}
+template< typename ... Args >
+std::string Karistir(Args const& ... args)
+{
+    std::ostringstream stream;
+    using List = int[];
+    (void)List {
+        0, ((void)(stream << args), 0) ...
+    };
+    return stream.str();
+}
+std::string HwidVer() {
+    //HW_PROFILE_INFO hwProfileInfo;
+    //GetCurrentHwProfile(&hwProfileInfo);
+    //std::string hwidWString = hwProfileInfo.szHwProfileGuid;
+    //std::string a = hwidWString.substr(1, 8), b = hwidWString.substr(10, 4), c = hwidWString.substr(15, 4);
+    //std::string karismis = Karistir(b, c, a);
+    std::string hwidWString;
+    DWORD serialNumber;
+    if (GetVolumeInformation(
+        "C:\\",
+        NULL,
+        0,
+        &serialNumber,
+        NULL,
+        NULL,
+        NULL,
+        0)) {
+        hwidWString = std::to_string(serialNumber);
+    }
+    return hwidWString;
+}
+std::string BeniSifrele(std::string text)
+{
+    return base64_encode(roatatealphabet(base64_encode(text.c_str())));
+}
+std::string BeniCoz(std::string text)
+{
+    return base64_decode(roatatealphabet(base64_decode(text.c_str())));
+}
---
+++ C++/LoginSystem/LoginSystem.cpp
@@ -0,0 +1,61 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <tchar.h>
+#include <random>
+#include <sstream>
+ #include <WinInet.h>
+#pragma comment(lib, "wininet.lib")
+#include "Auth.h"
+
+using namespace std;
+
+int ExpiryDays;
+string Anahtar, HwidAdresi, HWIDStr;
+
+ 
+int main() {
+    cout << "License: ";
+    cin >> Anahtar;
+    HwidAdresi = HwidVer();
+    string url = _xor_("http://5.180.106.192/licenseapi.php?license=").c_str() + BeniSifrele(Anahtar) + _xor_("&hwid=").c_str() + BeniSifrele(HwidAdresi) + _xor_("&expiryend=").c_str();
+    HINTERNET hInternet = InternetOpenA(_xor_("WebReader").c_str(), INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+    HINTERNET hConnect = InternetOpenUrlA(hInternet, url.c_str(), NULL, 0, INTERNET_FLAG_RELOAD, 0);
+    char buffer[4096];
+    DWORD bytesRead;
+    InternetReadFile(hConnect, buffer, sizeof(buffer), &bytesRead);
+    string strResult = BeniCoz(buffer).c_str();
+    size_t hwidPos = strResult.find(_xor_("HWID: ").c_str());
+    size_t daysPos = strResult.find(_xor_("Days: ").c_str());
+    if (hwidPos != std::string::npos && daysPos != std::string::npos) {
+        hwidPos += 6;
+        HWIDStr = strResult.substr(hwidPos, daysPos - hwidPos - 1);
+        daysPos += 6;
+        ExpiryDays = std::stoi(strResult.substr(daysPos));
+    }
+
+    if (strResult.find(_xor_("Invalid license").c_str()) != std::string::npos) {
+        cout << _xor_("Lisans bulunamadi.\n\n").c_str();
+        Sleep(5000);
+        exit(0);
+    }
+    else if (HWIDStr == HwidAdresi) {
+        cout << _xor_("Giris Basarili.\n\n").c_str();
+        std::cout << _xor_("Kalan Lisans: ").c_str() << ExpiryDays << _xor_(" Gun.\n\n").c_str() <<std::endl;
+    }
+    else if (strResult.find(_xor_("expiryed").c_str()) != std::string::npos) {
+        cout << _xor_("Lisans sureniz bitti.\n\n").c_str();
+        Sleep(5000);
+        exit(0);
+    }
+    else if (HWIDStr == _xor_("Banned").c_str()) {
+        cout << _xor_("Hesabiniz yasaklandi.\n\n").c_str();
+        Sleep(5000);
+        exit(0);
+    }
+    else {
+        cout << _xor_("HWID eslesmiyor.\n\n").c_str();
+        Sleep(5000);
+        exit(0);
+    }
+    system(_xor_("pause").c_str());
+}
